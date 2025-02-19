56ee49ce1276f408b21a3811414dbba625f21729
beckylakes <beckylake96@gmail.com>
2025-02-19 15:45:26 +0000

    Added new component utility classes for tailwind and edited fonts and logo for layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,8 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>
-    <Button variant="destructive" className="px-20">Hello</Button>
-  </main>;
+  return (
+    <main>
+    </main>
+  );
 }
