13e30377427e1a6174edaf1a708713b721098e43
beckylakes <beckylake96@gmail.com>
2025-02-19 21:56:33 +0000

    finished footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-2 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/full-logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>&#169; 2025 Eventure. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
