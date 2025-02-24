fa67c389dbd3d519f0614dcde92f52aa4f7b7117
beckylakes <beckylake96@gmail.com>
2025-02-24 14:13:29 +0000

    found more file routing errors - now fixed!

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,11 +2,11 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from '@/lib/database'
-import Event from '@/lib/database/models/event.model'
-import User from '@/lib/database/models/user.model'
-import Category from '@/lib/database/models/category.model'
-import { handleError } from '@/lib/utils'
+import { connectToDatabase } from '../mongodb/database/index'
+import Event from '../mongodb/database/models/event.model'
+import User from '../mongodb/database/models/user.model'
+import Category from '../mongodb/database/models/category.model'
+import { handleError } from '../utils'
 
 import {
   CreateEventParams,
