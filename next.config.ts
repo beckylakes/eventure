2f74e8a9febf5fa95a46d86b9e8953245ee1950c
beckylakes <beckylake96@gmail.com>
2025-02-27 23:01:17 +0000

    Added single event details page, and event card to homepage and working delete event function, now working on update event function

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'vtmgwotolt.ufs.sh',
+        port: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
